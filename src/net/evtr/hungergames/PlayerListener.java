9731ca8f43aa151678291f0bac4c7b7c492c1524
Mark Lohstroh <mlohstroh@gmail.com>
2012-04-19 22:37:56 -0500

    killed players can now go into spectator mode

---
+++ src/net/evtr/hungergames/PlayerListener.java
@@ -0,0 +1,29 @@
+package net.evtr.hungergames;
+
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDeathEvent;
+
+public class PlayerListener implements Listener
+{
+	HungerGames hgInstance;
+	
+	public PlayerListener(HungerGames instance)
+	{
+		hgInstance = instance;
+	}
+	
+	@EventHandler
+	public void PlayerDied(EntityDeathEvent event)
+	{
+		if (event.getEntity() instanceof Player)
+		{
+			//then a player died so let's add him to the list
+			hgInstance.killedPlayers.add((Player)event.getEntity());
+			Player player = (Player)event.getEntity();
+			player.sendMessage(ChatColor.AQUA + "You are now allowed to spectate! /s");
+		}
+	}
+}
